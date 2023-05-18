51bda65cab46f7ad0807939fff96232245a90a96
Jabar Sidik <jabar.sidik0617@gmail.com>
2023-05-18 21:05:44 +0700

    menambahkan shopping cart di bawah product

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
 const navbarnav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarnav.classList.toggle("active");
+  e.preventDefault();
 };
 
 // toggle class active untuk search form
@@ -11,19 +12,22 @@ const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 
 // ketika search form di klik
-document.querySelector("#search-button").onclick = ()=> {
-  searchForm.classList.toggle("active")
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
   searchBox.focus();
-  e,prevent();
+  e.preventDefault();
 };
 
-
-
-// klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarnav.contains(e.target)) {
     navbarnav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
