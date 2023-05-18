fce939d51a5bec61d702b2e0f54992ad1bd9fc58
Jabar Sidik <jabar.sidik0617@gmail.com>
2023-05-18 18:55:40 +0700

    menambahkan search form

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-// toggle class active
+// toggle class active untuk hamburger menu
 const navbarnav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarnav.classList.toggle("active");
 };
+
+// toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+// ketika search form di klik
+document.querySelector("#search-button").onclick = ()=> {
+  searchForm.classList.toggle("active")
+  searchBox.focus();
+  e,prevent();
+};
+
+
 
 // klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
